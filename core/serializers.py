b74b9d42b75596b1bd1721f86181c2c5d9dc278d
Adilson93 <adilsonebo@gmail.com>
2025-01-30 21:11:28 +0100

    commit-versao-test
    Nesta versão foi adicionado os seguintes passos:
    
    *conceito de permissões
    *permissões do drf
    *autenticação com Simple_JWT
    *teste de permissões das enpoints
    *habilitação do swagger
    *enviou de chamadas autenticadas no swagger
    *criação do atributo usuario a partir da requisição da comprar
    *filtro das compras com o usuario autenticado
    *validação de quantidade de intes em estoque
    *configuração do tempo de validade do token
    *

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,102 @@
-from rest_framework.serializers import ModelSerializer
-from core.models import Categoria
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
+from rest_framework import serializers
+from core.models import Categoria, Editora, Autor, Livro, Compra, ItensCompra
 
-class CategoriaSerializar(ModelSerializer):
+class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = '__all__'
+
+class EditoraNestedSerialazer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = ("nome",)
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+
+class LivroDetailSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao")
+    #editora = EditoraNestedSerialazer()
+    autores = SerializerMethodField()
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1    
+    
+    def get_autores(self, instace):
+        nomes_autores = []
+        autores = instace.autores.get_queryset()
+        for autor in autores:
+            nomes_autores.append(autor.nome)
+            return nomes_autores
+
+class ItemCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+    class Meta:
+        model= ItensCompra
+        fields = ("livro", "quantidade", "total")
+        depth = 1
+
+    def get_total(self, instace):
+        return instace.quantidade * instace.livro.preco
+
+class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email")
+    status = SerializerMethodField()
+    itens = ItemCompraSerializer(many=True)
+    class Meta:
+        model= Compra
+        fields= ["id", "status", "usuario", "itens", "total"]
+    
+    def get_status(self, instace):
+        return instace.get_status_display()
+    
+class CriarEditarItensCompraSerializar(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ['livro', 'quantidade']
+
+    def validate(self, data):
+        if data['quantidade'] > data['livro'].quantidade:
+            raise serializers.ValidationError({
+                    'quantidade': 'Quantidade solicitada não disponivel em estoque.'
+            })
+        return data
+
+
+class CriarEditarCompraSerializar(ModelSerializer):
+    itens = CriarEditarItensCompraSerializar(many=True)
+    usuario = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Compra
+        fields = ("id", "usuario", "itens")
+
+    def create(self, validated_data):
+        itens = validated_data.pop('itens', [])
+        compra = Compra.objects.create(**validated_data)
+        for item in itens:
+            ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return compra
+    
+    def update(self, instance, validated_data):
+        itens = validated_data.pop('itens')
+        if itens:
+            instance.itens.all().delete()
+            for item in itens:
+                ItensCompra.objects.create(compra=instance, **item)
+            instance.save()
+        return instance
---
+++ core/views/__init__.py
@@ -0,0 +1,9 @@
+from .testesIniciais import test
+from .categoriaClass import CategoriaView
+from .categoriaApiView import CategoriaDetail, CategoriasList
+from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+from .categoriaViewSet import CategoriaViewset
+from .editoraView import EditoraViewSet
+from .autorView import AutorViewset
+from .livroView import LivroViewSet
+from .compraView import CompraViewSet
